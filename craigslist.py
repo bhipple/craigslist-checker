4055e2045952e2f7df45ae0e7cbbd4be92570430
Jeehyun Kim <inneekim@gmail.com>
2015-06-27 21:25:17 -0400

    Refactored to camelcase, email

--- craigslist.py
+++ craigslist.py
@@ -11,7 +11,7 @@ import config
 BASE_URL = ('http://chicago.craigslist.org/search/'
             '?sort=rel&areaID=11&subAreaID=&query={0}&catAbb=sss')
 
-def parse_results(search_term):
+def parseResults(search_term):
     results = []
     search_term = search_term.strip().replace(' ', '+')
     search_url = BASE_URL.format(search_term)
@@ -25,7 +25,7 @@ def parse_results(search_term):
         results.append({'url': url, 'create_date': create_date, 'title': title})
     return results
 
-def write_results(results):
+def writeResults(results):
     """Writes list of dictionaries to file."""
     fields = results[0].keys()
     with open('results.csv', 'w') as f:
@@ -33,7 +33,7 @@ def write_results(results):
         dw.writer.writerow(dw.fieldnames)
         dw.writerows(results)
 
-def has_new_records(results):
+def hasNewRecords(results):
     current_posts = [x['url'] for x in results]
     fields = results[0].keys()
     if not os.path.exists('results.csv'):
@@ -51,7 +51,7 @@ def has_new_records(results):
             is_new = True
     return is_new
 
-def send_email(email_address, msg):
+def sendEmail(email_address, msg):
     fromaddr = "Craigslist Checker"
     toaddrs = email_address
     msg = ("From: {0}\r\nTo: {1}\r\n\r\n{2}").format(fromaddr, toaddrs, msg)
@@ -61,7 +61,11 @@ def send_email(email_address, msg):
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
 
-def get_current_time():
+def formatMsg(results, TERM):
+    message = "Hey - there are new Craigslist posts for: {0}".format(TERM.strip())
+    return message
+
+def getCurrentTime():
     return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
@@ -72,13 +76,14 @@ if __name__ == '__main__':
         print "You need to include a search term and an email address!\n"
         sys.exit(1)
 
-    results = parse_results(TERM)
+    results = parseResults(TERM)
 
     # Send the SMS message if there are new results
-    if has_new_records(results):
-        message = "Hey - there are new Craigslist posts for: {0}".format(TERM.strip())
-        print "[{0}] There are new results - sending email message to {0}".format(get_current_time(), EMAIL_ADDRESS)
-        send_email(EMAIL_ADDRESS, message)
-        write_results(results)
+    if hasNewRecords(results):
+        message = formatMsg(results, TERM)
+
+        print "[{0}] There are new results - sending email message to {0}".format(getCurrentTime(), EMAIL_ADDRESS)
+        sendEmail(EMAIL_ADDRESS, message)
+        writeResults(results)
     else:
-        print "[{0}] No new results - will try again later".format(get_current_time())
+        print "[{0}] No new results - will try again later".format(getCurrentTime())
